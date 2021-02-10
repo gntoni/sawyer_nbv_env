44cd015a510909eebddb86c59ee890fdaffd15da
Toni Gabas <toni2332@gmail.com>
2021-02-10 20:29:51 +0900

    include previously modified files

--- sawyer_nbv_env/__init__.py
+++ sawyer_nbv_env/__init__.py
@@ -1,1 +1,3 @@
 from sawyer_nbv_env.tasks.lift_nbv_cube import LiftNBVCube
+from sawyer_nbv_env.tasks.capture_dataset import CaptureDataset
+
--- sawyer_nbv_env/sawyer_env.py
+++ sawyer_nbv_env/sawyer_env.py
@@ -4,6 +4,32 @@ from robosuite.controllers import load_controller_config
 from gym.envs.registration import register
 from sawyer_nbv_env.wrappers import MergeObsWrapper, GymWrapper
 
+
+def CapDataEnv():
+    env = GymWrapper(
+        MergeObsWrapper(
+            rs.make(
+                "CaptureDataset",
+                robots=["Sawyer"],
+                controller_configs=load_controller_config(default_controller="OSC_POSITION"),            
+                use_camera_obs=True,  #  use pixel observations
+                has_offscreen_renderer=True,  # not needed since not using pixel obs
+                has_renderer=False,  # make sure we can render to the screen
+                reward_shaping=True,  # use dense rewards
+                control_freq=10,  # control should happen fast enough so that simulation looks smooth
+                camera_names= ["robot0_eye_in_hand"],
+                camera_heights=[84],
+                camera_widths=[84],
+                camera_depths=[True],
+            ),
+            img_keys = ["robot0_eye_in_hand_image", "robot0_eye_in_hand_depth"],
+            vect_keys = ["robot0_eef_pos","robot0_gripper_qpos"]
+        ),
+        keys=["merged_obs"]
+    )
+    return env
+
+
 def liftEnv():
     env = GymWrapper(
         MergeObsWrapper(
@@ -50,6 +76,12 @@ def liftEnvPlay():
         keys=["merged_obs"]        
     )
     return env
+
+register(
+    'CaptureDataset-v0',
+    entry_point='sawyer_nbv_env.sawyer_env:CapDataEnv',
+    max_episode_steps=500
+)
 
 register(
     'SawyerLift-v0',
